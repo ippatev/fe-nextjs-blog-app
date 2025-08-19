083cf5918c4f9427409f899184b051f3fe86c1e8
ippatev <alexanderippatiev24@gmail.com>
2025-08-19 11:45:48 +0900

    Initial commit

---
+++ apps/fe-nextjs-blog-app/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  displayName: '@blog/fe-nextjs-blog-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/fe-nextjs-blog-app',
+  testEnvironment: 'jsdom',
+};
+
+export default createJestConfig(config);
